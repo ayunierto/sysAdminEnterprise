21b6feeb2df125fce834c5a146cee65b3bc28d77
ayunierto <32336070+ayunierto@users.noreply.github.com>
2022-07-21 08:02:43 -0500

    add foreign key to categories table

--- database/migrations/2022_07_21_002456_create_categories_table.php
+++ database/migrations/2022_07_21_002456_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('companies_id');
+            $table->foreign('companies_id')->references('id')->on('companies');
             $table->string('name');
             $table->string('description')->nullable();
             $table->timestamps();
