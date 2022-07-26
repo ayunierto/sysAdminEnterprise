b8f14d3705434064ea00291a9e208cd83eefb8f6
ivanrv23 <ronalirv23@gmail.com>
2022-07-26 12:10:17 -0500

    modulo compras

--- plantillas/moduloCompras.js
+++ plantillas/moduloCompras.js
@@ -1,56 +1,88 @@
 new Vue({
     el: "#app",
     vuetify: new Vuetify(),
-    data: (vm) => ({
+    data: () => ({
         hasSaved: false,
         isEditing: null,
         // Fecha de formualrio
-        date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-            .toISOString()
-            .substr(0, 10),
-        dateFormatted: vm.formatDate(
-            new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-                .toISOString()
-                .substr(0, 10)
-        ),
         menu1: false,
+        dialog: false,
+        dialogDelete: false,
         tipoComprobante: [
             { name: "Ticket", abbr: "YT", id: 1 },
             { name: "Boleta", abbr: "GA", id: 2 },
             { name: "Factura", abbr: "AT", id: 3 },
         ],
+        desserts: [
+            {
+                producto: "Frozen Yogurt",
+                cantidad: 159,
+                precio: 50,
+            },
+            {
+                producto: "Ice cream sandwich",
+                cantidad: 237,
+                precio: 12,
+            },
+            {
+                producto: "Eclair",
+                cantidad: 262,
+                precio: 30,
+            },
+            {
+                producto: "Cupcake",
+                cantidad: 305,
+                precio: 15,
+            },
+        ],
     }),
-    computed: {
-        computedDateFormatted() {
-            return this.formatDate(this.date);
+    watch: {
+        dialog(val) {
+            val || this.close();
+        },
+        dialogDelete(val) {
+            val || this.closeDelete();
         },
     },
-
-    watch: {
-        // fecha
-        date(val) {
-            this.dateFormatted = this.formatDate(this.date);
+    methods: {
+        save() {
+            this.isEditing = !this.isEditing;
+            this.hasSaved = true;
+            if (this.editedIndex > -1) {
+                Object.assign(this.desserts[this.editedIndex], this.editedItem);
+            } else {
+                this.desserts.push(this.editedItem);
+            }
+            this.close();
         },
-        methods: {
-            formatDate(date) {
-                if (!date) return null;
+        close() {
+            this.dialog = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
+        editItem(item) {
+            this.editedIndex = this.desserts.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialog = true;
+        },
 
-                const [year, month, day] = date.split("-");
-                return `${month}/${day}/${year}`;
-            },
-            parseDate(date) {
-                if (!date) return null;
-
-                const [month, day, year] = date.split("/");
-                return `${year}-${month.padStart(2, "0")}-${day.padStart(
-                    2,
-                    "0"
-                )}`;
-            },
-            save() {
-                this.isEditing = !this.isEditing;
-                this.hasSaved = true;
-            },
+        deleteItem(item) {
+            this.editedIndex = this.desserts.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialogDelete = true;
+        },
+        deleteItemConfirm() {
+            this.desserts.splice(this.editedIndex, 1);
+            this.closeDelete();
+        },
+        closeDelete() {
+            this.dialogDelete = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
         },
     },
 });
