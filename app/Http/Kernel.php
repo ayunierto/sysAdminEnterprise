4f798defe387549d51394c40c2dd560ec03da75d
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-07-31 12:07:28 -0500

    adding middleware for permissions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'CheckMaster' => \App\Http\Middleware\CheckMaster::class,
+        'CheckAdmin' => \App\Http\Middleware\CheckAdmin::class,
     ];
 }
