01becc7fea0fcb0e6bd83ac07c279554991facb5
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-07-31 11:58:19 -0500

    identifying code

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -9,20 +9,17 @@
         <div v-if="$page.props.errorBags.default">
             <v-alert type="warning" border="left" dismissible 
              v-for="item in $page.props.errorBags.default.company" 
-             :key="$page.props.errorBags.default.company[0]"   
-            >
+             :key="$page.props.errorBags.default.company[0]">
                 {{ item }}
             </v-alert>
              <v-alert type="warning" border="left" dismissible 
              v-for="item in $page.props.errorBags.default.name" 
-             :key="$page.props.errorBags.default.name[0]"   
-            >
+             :key="$page.props.errorBags.default.name[0]">
                 {{ item }}
             </v-alert>
             <v-alert type="warning" border="left" dismissible 
              v-for="item in $page.props.errorBags.default.description" 
-             :key="$page.props.errorBags.default.description[0]"   
-            >
+             :key="$page.props.errorBags.default.description[0]">
                 {{ item }}
             </v-alert>
         </div>
