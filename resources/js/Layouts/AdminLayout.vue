5924aeb9a0a04048cd146874730d598a9c5eedb2
ramos <ronalirv23@gmail.com>
2022-07-30 23:49:23 -0500

    Logo y Color Principal Empresa

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -3,32 +3,32 @@
         <!-- memú vertical y horizontal -->
         <template>
             <!-- Menú vertical -->
-            <v-navigation-drawer app v-model="drawer" v-bind="color">
+            <!-- <v-navigation-drawer app v-model="drawer" v-bind="color"> -->
+            <v-navigation-drawer app v-model="drawer" color="#1465bb">
                 <!-- cambio colores -->
                 <!-- v-bind:class="{ red: colorrojo}" -->
-                <v-list>
+                <v-list color="#1465bb">
                     <v-list-item class="px-2">
-                        <v-list-item-avatar>
-                            <v-img
-                                src="https://www.zarla.com/images/zarla-soluciona-1x1-2400x2400-20210603-xv6xrmkbpk3ggkjb6869.png?crop=1:1,smart&width=250&dpr=2">
+                        <v-avatar size="70">
+                            <v-img src="../img/empresa1/ecsmac8.png">
                             </v-img>
-                        </v-list-item-avatar>
-                        <v-list-item-title class="text-h6">ALPA PERÚ</v-list-item-title>
+                        </v-avatar>
+                        <v-list-item-title class="text-h6" style="color:white;">ECSMAC EIRL</v-list-item-title>
                     </v-list-item>
 
-                    <v-list-item link>
+                    <v-list-item>
                         <v-list-item-content>
-                            <v-list-item-subtitle>
-                                correo@gmail.com
+                            <v-list-item-subtitle style="color:white;">
+                                CONSTRUYENDO FUTUROS
                             </v-list-item-subtitle>
                         </v-list-item-content>
 
                         <!-- Personalizar colores -->
-                        <v-fab-transition>
+                        <!-- <v-fab-transition>
                             <v-btn v-show="!hidden" color="pink" fab dark small absolute bottom right>
-                                <v-icon>mdi-spin mdi-cog</v-icon>
+                                <v-icon color="white">mdi-spin mdi-cog</v-icon>
                             </v-btn>
-                        </v-fab-transition>
+                        </v-fab-transition> -->
 
                     </v-list-item>
                 </v-list>
@@ -37,12 +37,12 @@
 
                 <!-- CONTENIDO MENU VERTICAL -->
                 <v-list nav dense>
-                    <v-list-item-group color="primary">
+                    <v-list-item-group>
 
                         <inertia-link :href="route('dashboard')">
-                            <v-list-item link>
+                            <v-list-item link style="color:white;">
                                 <v-list-item-icon>
-                                    <v-icon>mdi-monitor-multiple</v-icon>
+                                    <v-icon color="white">mdi-monitor-multiple</v-icon>
                                 </v-list-item-icon>
                                 <v-list-item-content>
                                     <v-list-item-title>INICIO</v-list-item-title>
@@ -50,38 +50,38 @@
                             </v-list-item>
                         </inertia-link>
 
-                        <v-list-item link>
+                        <v-list-item link style="color:white;">
                             <v-list-item-icon>
-                                <v-icon>mdi-cart</v-icon>
+                                <v-icon color="white">mdi-cart</v-icon>
                             </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title>VENTAS</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
-                        <v-list-item link>
+                        <v-list-item link style="color:white;">
                             <v-list-item-icon>
-                                <v-icon>mdi-cash-multiple</v-icon>
+                                <v-icon color="white">mdi-cash-multiple</v-icon>
                             </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title>COMPRAS</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
-                        <v-list-group prepend-icon="mdi-spin mdi-cube" color="primary">
-
+                        <v-list-group>
+                            <v-icon slot="prependIcon" color="white">mdi-spin mdi-cube</v-icon>
                             <template v-slot:activator>
-                                <v-list-item-content>
+                                <v-list-item-content style="color:white;">
                                     <v-list-item-title>PRODUCTOS</v-list-item-title>
                                 </v-list-item-content>
                             </template>
 
-                            <v-list-item-group color="primary">
+                            <v-list-item-group >
 
                                 <inertia-link :href="route('products.index')">
-                                    <v-list-item link>
+                                    <v-list-item link style="color:white;">
                                         <v-list-item-icon>
                                             <v-spacer></v-spacer>
-                                            <v-icon small>mdi-format-list-bulleted</v-icon>
+                                            <v-icon color="white" small>mdi-format-list-bulleted</v-icon>
                                         </v-list-item-icon>
 
                                         <v-list-item-title>
@@ -92,10 +92,10 @@
                                     </v-list-item>
                                 </inertia-link>
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-cube-send</v-icon>
+                                        <v-icon color="white" small>mdi-cube-send</v-icon>
                                     </v-list-item-icon>
 
                                     <v-list-item-title>
@@ -106,10 +106,10 @@
                                 </v-list-item>
 
                                 <inertia-link :href="route('categories.index')">
-                                    <v-list-item link>
+                                    <v-list-item link style="color:white;">
                                         <v-list-item-icon>
                                             <v-spacer></v-spacer>
-                                            <v-icon small>mdi-book-variant</v-icon>
+                                            <v-icon color="white" small>mdi-book-variant</v-icon>
                                         </v-list-item-icon>
                                         <v-list-item-title>
                                             <h5>CATEGORIAS</h5>
@@ -118,10 +118,10 @@
                                 </inertia-link>
 
                                 <inertia-link :href="route('marks.index')">
-                                    <v-list-item link>
+                                    <v-list-item link style="color:white;">
                                         <v-list-item-icon>
                                             <v-spacer></v-spacer>
-                                            <v-icon small>mdi-bookmark-plus</v-icon>
+                                            <v-icon color="white" small>mdi-bookmark-plus</v-icon>
                                         </v-list-item-icon>
                                         <v-list-item-title>
                                             <h5>MARCAS</h5>
@@ -130,30 +130,30 @@
                                 </inertia-link>
 
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-currency-usd</v-icon>
+                                        <v-icon color="white" small>mdi-currency-usd</v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-title>
                                         <h5>PRECIOS DE PRODUCTOS</h5>
                                     </v-list-item-title>
                                 </v-list-item>
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-package-variant-closed</v-icon>
+                                        <v-icon color="white" small>mdi-package-variant-closed</v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-title>
                                         <h5>PRESENTACIÓN DE PRODUCTOS</h5>
                                     </v-list-item-title>
                                 </v-list-item>
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-package-variant</v-icon>
+                                        <v-icon color="white" small>mdi-package-variant</v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-title>
                                         <h5>LOTES DE PRODUCTOS</h5>
@@ -163,18 +163,18 @@
                             </v-list-item-group>
                         </v-list-group>
 
-                        <v-list-item link>
+                        <v-list-item link style="color:white;">
                             <v-list-item-icon>
-                                <v-icon>mdi-account-group</v-icon>
+                                <v-icon color="white">mdi-account-group</v-icon>
                             </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title>CLIENTES</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
-                        <v-list-item link>
+                        <v-list-item link style="color:white;">
                             <v-list-item-icon>
-                                <v-icon>mdi-account-heart-outline</v-icon>
+                                <v-icon color="white">mdi-account-heart-outline</v-icon>
                             </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title>EMPLEADOS</v-list-item-title>
@@ -182,9 +182,9 @@
                         </v-list-item>
 
                         <inertia-link :href="route('providers.index')">
-                            <v-list-item link>
+                            <v-list-item link style="color:white;">
                                 <v-list-item-icon>
-                                    <v-icon>mdi-account-hard-hat</v-icon>
+                                    <v-icon color="white">mdi-account-hard-hat</v-icon>
                                 </v-list-item-icon>
                                 <v-list-item-content>
                                     <v-list-item-title>PROVEEDORES</v-list-item-title>
@@ -192,49 +192,49 @@
                             </v-list-item>
                         </inertia-link>
 
-                        <v-list-item link>
+                        <v-list-item link style="color:white;">
                             <v-list-item-icon>
-                                <v-icon>mdi-spin mdi-cog</v-icon>
+                                <v-icon color="white">mdi-spin mdi-cog</v-icon>
                             </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title>AJUSTES</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
-                        <v-list-group prepend-icon="mdi-account-tie" color="primary">
-
+                        <v-list-group>
+                            <v-icon slot="prependIcon" color="white">mdi-account-tie</v-icon>
                             <template v-slot:activator>
-                                <v-list-item-content>
+                                <v-list-item-content style="color:white;">
                                     <v-list-item-title>ADMINISTRACION</v-list-item-title>
                                 </v-list-item-content>
                             </template>
 
-                            <v-list-item-group color="primary">
+                            <v-list-item-group >
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-office-building</v-icon>
+                                        <v-icon color="white" small>mdi-office-building</v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-title>
                                         <h5>EMPRESAS</h5>
                                     </v-list-item-title>
                                 </v-list-item>
 
-                                <v-list-item link>
+                                <v-list-item link style="color:white;">
                                     <v-list-item-icon>
                                         <v-spacer></v-spacer>
-                                        <v-icon small>mdi-account-key</v-icon>
+                                        <v-icon color="white" small>mdi-account-key</v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-title>
                                         <h5>ACCESOS</h5>
                                     </v-list-item-title>
                                 </v-list-item>
                                 <inertia-link :href="route('measures.index')">
-                                    <v-list-item link>
+                                    <v-list-item link style="color:white;">
                                         <v-list-item-icon>
                                             <v-spacer></v-spacer>
-                                            <v-icon small>mdi-weight-kilogram</v-icon>
+                                            <v-icon color="white" small>mdi-weight-kilogram</v-icon>
                                         </v-list-item-icon>
                                         <v-list-item-title>
                                             <h5>UNIDADES DE MEDIDA</h5>
@@ -253,8 +253,9 @@
 
 
             <!-- Menú horizontal -->
-            <v-app-bar app v-bind="color">
-                <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <!-- <v-app-bar app v-bind="color"> -->
+            <v-app-bar app color="#1465bb">
+                <v-app-bar-nav-icon @click="drawer = !drawer" color="white"></v-app-bar-nav-icon>
 
                 <v-toolbar-title>
                     <!-- INICIO -->
@@ -268,8 +269,8 @@
                 </v-list-item-avatar>
                 <v-menu left bottom>
                     <template v-slot:activator="{ on, attrs }">
-                        <v-btn icon v-bind="attrs" v-on="on">
-                            <v-icon>mdi-dots-vertical</v-icon>
+                        <v-btn icon v-bind="attrs" v-on="on" color="white">
+                            <v-icon color="white">mdi-dots-vertical</v-icon>
                         </v-btn>
                     </template>
                     <v-list>
@@ -292,6 +293,7 @@
             <pre>{{ $page }}</pre>
         </v-main>
         <!-- PIE DE PAGINA -->
+        <!-- <v-footer padless v-bind="color" height="60px"> -->
         <v-footer padless v-bind="color" height="60px">
             <v-col class="text-center" cols="12" absolute>
                 {{ new Date().getFullYear() }} — <strong>ALPA PERÚ</strong>
@@ -307,9 +309,9 @@ export default {
         return {
             drawer: null,
             hidden: false,
-            color: {
-                color: "blue",
-            }
+            // color: {
+            //     color: "blue",
+            // }
         };
     },
 };
