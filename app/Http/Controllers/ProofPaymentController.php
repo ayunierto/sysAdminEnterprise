3b66b1b0a161c4e4ecfac150fbe8ad27086f05e6
ivanrv23 <ronalirv23@gmail.com>
2022-08-02 12:50:05 -0500

    add crud tipos comprobantes

---
+++ app/Http/Controllers/ProofPaymentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProofPayment;
+use App\Http\Requests\StoreProofPaymentRequest;
+use App\Http\Requests\UpdateProofPaymentRequest;
+
+class ProofPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProofPaymentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProofPaymentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProofPayment  $proofPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProofPayment $proofPayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProofPayment  $proofPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProofPayment $proofPayment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProofPaymentRequest  $request
+     * @param  \App\Models\ProofPayment  $proofPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProofPaymentRequest $request, ProofPayment $proofPayment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProofPayment  $proofPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProofPayment $proofPayment)
+    {
+        //
+    }
+}
