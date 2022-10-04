441092934f8863ce6e1ac7e8955709682ff6c821
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-10-04 13:02:25 -0500

    tikets

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -44,34 +44,34 @@ class PrintController extends Controller
 
 
 
-/**
- * Install the printer using USB printing support, and the "Generic / Text Only" driver,
- * then share it (you can use a firewall so that it can only be seen locally).
- *
- * Use a WindowsPrintConnector with the share name to print.
- *
- * Troubleshooting: Fire up a command prompt, and ensure that (if your printer is shared as
- * "Receipt Printer), the following commands work:
- *
- *  echo "Hello World" > testfile
- *  copy testfile "\\%COMPUTERNAME%\Receipt Printer"
- *  del testfile
- */
-try {
-    // Enter the share name for your USB printer here
-    // $connector = null;
-    $connector = new WindowsPrintConnector("Microsoft Print to PDF");
+        /**
+         * Install the printer using USB printing support, and the "Generic / Text Only" driver,
+         * then share it (you can use a firewall so that it can only be seen locally).
+         *
+         * Use a WindowsPrintConnector with the share name to print.
+         *
+         * Troubleshooting: Fire up a command prompt, and ensure that (if your printer is shared as
+         * "Receipt Printer), the following commands work:
+         *
+         *  echo "Hello World" > testfile
+         *  copy testfile "\\%COMPUTERNAME%\Receipt Printer"
+         *  del testfile
+         */
+        try {
+            // Enter the share name for your USB printer here
+            // $connector = null;
+            $connector = new WindowsPrintConnector("Microsoft Print to PDF");
 
-    /* Print a "Hello world" receipt" */
-    $printer = new Printer($connector);
-    $printer -> text("Hello World!\n");
-    $printer -> cut();
-    
-    /* Close printer */
-    $printer -> close();
-} catch (Exception $e) {
-    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
-}
+            /* Print a "Hello world" receipt" */
+            $printer = new Printer($connector);
+            $printer -> text("Hello World!\n");
+            $printer -> cut();
+            
+            /* Close printer */
+            $printer -> close();
+        } catch (Exception $e) {
+            echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+        }
         
         // return  $order_details;      
         // return view('pdf.invoice', compact('order', 'order_details', 'company', 'customizer'));
