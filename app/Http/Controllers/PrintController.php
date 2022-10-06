1d3ae15e3dc183f00c61b2560a7e38ff1c6166f1
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-10-06 15:34:21 -0500

    logica para tikets implementada

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -11,6 +11,7 @@ use App\Models\Product;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Exception;
 use Illuminate\Support\Facades\Auth;
+use App\Custom\Item;
 
 // Printer
 use Mike42\Escpos\Printer;
@@ -39,39 +40,118 @@ class PrintController extends Controller
             ];
         });
 
-
-
-
-
-
-        /**
-         * Install the printer using USB printing support, and the "Generic / Text Only" driver,
-         * then share it (you can use a firewall so that it can only be seen locally).
-         *
-         * Use a WindowsPrintConnector with the share name to print.
-         *
-         * Troubleshooting: Fire up a command prompt, and ensure that (if your printer is shared as
-         * "Receipt Printer), the following commands work:
-         *
-         *  echo "Hello World" > testfile
-         *  copy testfile "\\%COMPUTERNAME%\Receipt Printer"
-         *  del testfile
-         */
         try {
             // Enter the share name for your USB printer here
+            $nombre_impresora = "Microsoft Print to PDF";
             // $connector = null;
-            $connector = new WindowsPrintConnector("Microsoft Print to PDF");
+            $connector = new WindowsPrintConnector($nombre_impresora);
 
             /* Print a "Hello world" receipt" */
             $printer = new Printer($connector);
-            $printer -> text("Hello World!\n");
-            $printer -> cut();
+
+            // Vamos a alinear al centro lo próximo que imprimamos
+            $printer -> setJustification(Printer::JUSTIFY_CENTER);
+            // Encabezado
+            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+            $printer -> text( $company->name . "\n");
+
+            $printer -> selectPrintMode();
+
+            $printer -> text( $company->address . "\n");
+            $printer -> text( $company->phone . "\n");
+            $printer -> text( "R.U.C." . $company->ruc . "\n");
+
+            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);            
+
+            $comprobante = $order->proof_payments_id;
+            if ($comprobante == 1) {
+                $printer -> text( "COMPROBANTE" . "\n");
+            }
+            if ($comprobante == 2) {
+                $printer -> text( "FACTURA" . "\n");
+            }
+            if ($comprobante == 3) {
+                $printer -> text( "BOLETA DE VENTA" . "\n");
+            }
+
+            $printer -> selectPrintMode();
+
+            // Imprimir fecha
+            $printer->text(date("Y-m-d H:i:s") . "\n");
+
+            $printer->text("--------------------------------------------". "\n");
+
+            // Imprimir productos
+            $total = 0;
+
+            $printer->setJustification(Printer::JUSTIFY_LEFT);
+
+            //$items[] = array();
+            $items[] = new Item("Cantidad","Descripción","Presentación","Precio", "IGV", "Total");
+
+            foreach ($order_details as $value) {
+                //$printer->text($value['nombre'] ."\t".$value['peso_cantidad'] ."\t".$value['precio'] ."\t".$value['preciototal']."\n");
+                $items[] = new item($value['quantity'],$value['product'] ,$value['presentation'] ,$value['price'], $value['igv'], $value['subTotal']);
+                $total += $value['subTotal'];
+            }
+            $i = 0;
+            foreach ($items as $item) {
+                if($i == 0){
+                    //$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+                    $printer -> setJustification(Printer::JUSTIFY_CENTER);
+                    $printer -> text($item);
+                    $printer->text("--------------------------------------------". "\n");
+                    $printer -> selectPrintMode();
             
-            /* Close printer */
-            $printer -> close();
+                }else{
+                    $printer -> text($item);
+                }
+                $i++;
+            }
+            
+            /*
+                Terminamos de imprimir
+                los productos, ahora va el total
+            */
+            $printer->text("----------------------\n");
+            $printer->text("TOTAL: Bs". $total ."\n");
+            // $printer->text("SON ". $total_literal ."\n");
+
+            $printer->text("Muchas gracias por su preferencia...");
+
+            /*Alimentamos el papel 3 veces*/
+            $printer->feed(3);
+
+            /*
+                Cortamos el papel. Si nuestra impresora
+                no tiene soporte para ello, no generará
+                ningún error
+            */
+            $printer->cut();
+
+            /*
+                Por medio de la impresora mandamos un pulso.
+                Esto es útil cuando la tenemos conectada
+                por ejemplo a un cajón
+            */
+            $printer->pulse();
+
+            /*
+                Para imprimir realmente, tenemos que "cerrar"
+                la conexión con la impresora. Recuerda incluir esto al final de todos los archivos
+            */
+            $printer->close();
         } catch (Exception $e) {
             echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
         }
+
+
+
+
+
+
+        // /* Clase de para organizar las columnas */
+        
         
         // return  $order_details;      
         // return view('pdf.invoice', compact('order', 'order_details', 'company', 'customizer'));
