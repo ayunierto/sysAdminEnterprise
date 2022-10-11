3fbbc1c6ee85db8df62e4ec585a9d68160a9fe70
ivanrv23 <ronalirv23@gmail.com>
2022-10-11 17:05:33 -0500

    cargar nombre de la impresora desde DB

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -12,29 +12,30 @@ use Barryvdh\DomPDF\Facade\Pdf;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use App\Custom\Item;
-
+use App\Models\PrintingMachine;
 // Printer
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
 class PrintController extends Controller
 {
-    
-    public function invoice( $rid = null){
+
+    public function invoice($rid = null)
+    {
         // Obteniendo el id enviado por url
-        
+
         if ($rid == null) {
             $id = $_REQUEST['id'];
         } else {
-             $id = $rid;
+            $id = $rid;
         }
 
         $company = Company::find(Auth::user()->companies_id);
         $customizer = Customizer::where('companies_id', Auth::user()->companies_id)->get()[0];
-        $total = 0;        
+        $total = 0;
         $order = Order::find($id); // Buscando venta
-        $order_details = OrderDetail::where('orders_id', $order->id)->get()->map(function ($o)
-        {
+        $impresora = PrintingMachine::where('companies_id', Auth::user()->companies_id)->where('state', 1)->get()[0];
+        $order_details = OrderDetail::where('orders_id', $order->id)->get()->map(function ($o) {
             return [
                 'quantity' => $o->quantity,
                 'product' => Product::find($o->products_id)->name,
@@ -47,7 +48,7 @@ class PrintController extends Controller
 
         try {
             // Enter the share name for your USB printer here
-            $nombre_impresora = "Microsoft Print to PDF";
+            $nombre_impresora = $impresora->name;
             // $connector = null;
             $connector = new WindowsPrintConnector($nombre_impresora);
 
@@ -55,36 +56,36 @@ class PrintController extends Controller
             $printer = new Printer($connector);
 
             // Vamos a alinear al centro lo próximo que imprimamos
-            $printer -> setJustification(Printer::JUSTIFY_CENTER);
+            $printer->setJustification(Printer::JUSTIFY_CENTER);
             // Encabezado
-            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-            $printer -> text( $company->name . "\n");
+            $printer->selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+            $printer->text($company->name . "\n");
 
-            $printer -> selectPrintMode();
+            $printer->selectPrintMode();
 
-            $printer -> text( $company->address . "\n");
-            $printer -> text( $company->phone . "\n");
-            $printer -> text( "R.U.C." . $company->ruc . "\n");
+            $printer->text($company->address . "\n");
+            $printer->text($company->phone . "\n");
+            $printer->text("R.U.C." . $company->ruc . "\n");
 
-            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);            
+            $printer->selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
 
             $comprobante = $order->proof_payments_id;
             if ($comprobante == 1) {
-                $printer -> text( "COMPROBANTE" . "\n");
+                $printer->text("COMPROBANTE" . "\n");
             }
             if ($comprobante == 2) {
-                $printer -> text( "FACTURA" . "\n");
+                $printer->text("FACTURA" . "\n");
             }
             if ($comprobante == 3) {
-                $printer -> text( "BOLETA DE VENTA" . "\n");
+                $printer->text("BOLETA DE VENTA" . "\n");
             }
 
-            $printer -> selectPrintMode();
+            $printer->selectPrintMode();
 
             // Imprimir fecha
             $printer->text(date("Y-m-d H:i:s") . "\n");
 
-            $printer->text("--------------------------------------------". "\n");
+            $printer->text("--------------------------------------------" . "\n");
 
             // Imprimir productos
             $total = 0;
@@ -92,34 +93,33 @@ class PrintController extends Controller
             $printer->setJustification(Printer::JUSTIFY_LEFT);
 
             //$items[] = array();
-            $items[] = new Item("Cantidad","Descripción","Presentación","Precio", "IGV", "Total");
+            $items[] = new Item("Cantidad", "Descripción", "Presentación", "Precio", "IGV", "Total");
 
             foreach ($order_details as $value) {
                 //$printer->text($value['nombre'] ."\t".$value['peso_cantidad'] ."\t".$value['precio'] ."\t".$value['preciototal']."\n");
-                $items[] = new item($value['quantity'],$value['product'] ,$value['presentation'] ,$value['price'], $value['igv'], $value['subTotal']);
+                $items[] = new item($value['quantity'], $value['product'], $value['presentation'], $value['price'], $value['igv'], $value['subTotal']);
                 $total += $value['subTotal'];
             }
             $i = 0;
             foreach ($items as $item) {
-                if($i == 0){
+                if ($i == 0) {
                     //$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-                    $printer -> setJustification(Printer::JUSTIFY_CENTER);
-                    $printer -> text($item);
-                    $printer->text("--------------------------------------------". "\n");
-                    $printer -> selectPrintMode();
-            
-                }else{
-                    $printer -> text($item);
+                    $printer->setJustification(Printer::JUSTIFY_CENTER);
+                    $printer->text($item);
+                    $printer->text("--------------------------------------------" . "\n");
+                    $printer->selectPrintMode();
+                } else {
+                    $printer->text($item);
                 }
                 $i++;
             }
-            
+
             /*
                 Terminamos de imprimir
                 los productos, ahora va el total
             */
             $printer->text("----------------------\n");
-            $printer->text("TOTAL: ". $total ."\n");
+            $printer->text("TOTAL: " . $total . "\n");
             // $printer->text("SON ". $total_literal ."\n");
 
             $printer->text("Muchas gracias por su preferencia...");
@@ -148,8 +148,7 @@ class PrintController extends Controller
             $printer->close();
         } catch (Exception $e) {
 
-            echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
-
+            echo "Couldn't print to this printer: " . $e->getMessage() . "\n";
         }
 
 
@@ -158,13 +157,13 @@ class PrintController extends Controller
 
 
         // /* Clase de para organizar las columnas */
-        
-        
+
+
         // return  $order_details;      
         // return view('pdf.invoice', compact('order', 'order_details', 'company', 'customizer'));
-        
+
         // $pdf = Pdf::loadView('pdf.invoice', compact('order', 'order_details', 'company', 'customizer', 'total'));
-        
+
         // return $pdf->download('invoice.pdf');
         // return $pdf->stream('invoice.pdf');
     }
