1b05298f2bd7c978664d6fceab87bce7276e289d
ivanrv23 <ronalirv23@gmail.com>
2022-10-13 13:04:53 -0500

    reportes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coin;
 use App\Models\Company;
+use App\Models\Customer;
 use App\Models\Customizer;
+use App\Models\Document;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\PaymentMethod;
+use App\Models\Product;
+use App\Models\ProofPayment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class ReportController extends Controller
 {
@@ -18,7 +27,75 @@ class ReportController extends Controller
     public function index()
     {
         $company = Auth::user()->companies_id;
+        $DateAndTime = date('Y-m-d');
+        $totalVentasDiaSoles = 0;
+        $totalVentasDiaDolares = 0;
+        $total_pen_usd = 0;
+
+        $total_pen = Order::where('companies_id', $company)->where('date', $DateAndTime)
+            ->where('coins_id', 1)->get();
+
+        foreach ($total_pen as $key => $p) {
+            $totalVentasDiaSoles += $p->total;
+        }
+        $total_usd = Order::where('companies_id', $company)->where('date', $DateAndTime)
+            ->where('coins_id', 2)->get();
+
+        foreach ($total_usd as $key => $p) {
+            $totalVentasDiaDolares += ($p->total * $p->exchange_rate);
+        }
+        $total_pen_usd =number_format(($totalVentasDiaSoles + round($totalVentasDiaDolares)), 2) ;
+
+        // Obntener ganancias del dia
+        $datosDetalle=array();
+        $totalsasd=0;
+        $ordertot=Order::where('companies_id', $company)->where('date', $DateAndTime)->get();
+        foreach ($ordertot as $key => $p) {
+            array_push($datosDetalle,OrderDetail::where('orders_id', $p->id)->get());
+        }
         return Inertia::render('Reports/Index', [
+            'totalVentas' => $total_pen_usd,
+            'valores'=>$datosDetalle,
+            'totOrders' => Order::where('companies_id', $company)->where('date', $DateAndTime)->count(),
+            'orders' => Order::where('companies_id', $company)->where('date', $DateAndTime)->get()->map(function ($p) {
+                return [
+                    'id' => $p->id,
+                    'companies_id' => $p->companies_id,
+                    'company_name' => Company::find($p->companies_id)->name,
+                    'customers_id' => $p->customers_id,
+                    'customers_name' => Customer::find($p->customers_id)->name,
+                    'payment_methods_id' => $p->payment_methods_id,
+                    'payment_method' => PaymentMethod::find($p->payment_methods_id)->description,
+                    'proof_payments_id' => $p->proof_payments_id,
+                    'proof_payment' => ProofPayment::find($p->proof_payments_id)->name,
+                    'coins_id' => $p->coins_id,
+                    'coin' => Coin::find($p->coins_id)->code,
+                    'documents_id' => $p->documents_id,
+                    'documents_name' => Document::find($p->documents_id)->name,
+                    'voucher_number' => $p->voucher_number,
+                    'exchange_rate' => $p->exchange_rate,
+                    'total' => $p->total,
+                    'date' => $p->date,
+                    'state' => $p->state,
+                    'state_name' => $p->state == 1 ? 'Registrado' : 'Pendiente',
+                    'description' => $p->description,
+                    'details' => OrderDetail::where('orders_id', $p->id)->get()->map(function ($d) {
+                        return [
+                            'id' => $d->id,
+                            'orders_id' => $d->orders_id,
+                            'products_id' => $d->products_id,
+                            'product_name' => Product::find($d->products_id)->name,
+                            'product_purchase_price' => Product::find($d->products_id)->purchase_price,
+                            'quantity' => $d->quantity,
+                            'price' => $d->price,
+                            'discount' => $d->discount,
+                            'igv' => $d->igv,
+                            'subTotal' => $d->subTotal,
+                        ];
+                        
+                    }),
+                ];
+            }),
             'colors' => Customizer::where('companies_id', $company)->get(),
             'company' => Company::find(Auth::user()->companies_id),
         ]);
--- resources/js/Pages/Reports/Index.vue
+++ resources/js/Pages/Reports/Index.vue
@@ -27,11 +27,11 @@
                                                 N° VENTAS
                                             </div>
                                             <v-list-item-title>
-                                                5
+                                                {{totOrders}}
                                             </v-list-item-title>
                                         </v-list-item-content>
 
-                                        <v-list-item-avatar tile size="50">
+                                        <v-list-item-avatar tile size="30">
                                             <v-img contain src="/img/dashboard/ventas.png">
                                             </v-img>
                                         </v-list-item-avatar>
@@ -41,15 +41,15 @@
                                     <v-list-item three-line>
                                         <v-list-item-content>
                                             <div>
-                                                MONTO VENTAS
+                                                TOTAL VENTAS
                                             </div>
                                             <v-list-item-title>
-                                                5
+                                                S/. {{totalVentas}}
                                             </v-list-item-title>
                                         </v-list-item-content>
 
-                                        <v-list-item-avatar tile size="50">
-                                            <v-img contain src="/img/dashboard/ventas.png">
+                                        <v-list-item-avatar tile size="30">
+                                            <v-img contain src="/img/dashboard/totalVentas.png">
                                             </v-img>
                                         </v-list-item-avatar>
                                     </v-list-item>
@@ -61,12 +61,12 @@
                                                 GANANCIAS
                                             </div>
                                             <v-list-item-title>
-                                                5
-                                            </v-list-item-title>
+                                                {{valores}}
+                                            </v-list-item-title>                                            
                                         </v-list-item-content>
 
-                                        <v-list-item-avatar tile size="50">
-                                            <v-img contain src="/img/dashboard/ventas.png">
+                                        <v-list-item-avatar tile size="30">
+                                            <v-img contain src="/img/dashboard/ganancias.png">
                                             </v-img>
                                         </v-list-item-avatar>
                                     </v-list-item>
@@ -74,43 +74,231 @@
                             </v-row>
                         </v-container>
                     </v-card>
-                    <v-data-table :headers="ventasDia" :items="desserts" sort-by="name" class="elevation-24"
+                    <v-data-table :headers="ventasDia" :items="dessertsVentas" sort-by="name" class="elevation-24"
                         :search="search">
+                        <template v-slot:top>
+                            <v-toolbar flat>
+                                <v-spacer></v-spacer>
+                                <v-text-field v-model="search" append-icon="mdi-magnify" label="Buscar" outlined dense
+                                    hide-details>
+                                </v-text-field>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                        </template>
+                        <!-- Acciones -->
+                        <template v-slot:[`item.actions`]="{ item }">
+                            <v-icon small class="mr-2" @click="printItem(item)">
+                                mdi-printer
+                            </v-icon>
+                            <v-icon small class="mr-2" @click="viewItem(item)">
+                                mdi-eye
+                            </v-icon>
+                        </template>
                     </v-data-table>
                 </v-tab-item>
+                <!-- Ver detalles Ventas -->
+                <template>
+                    <v-row justify="center">
+                        <v-dialog v-model="dialog_view" max-width="700">
+
+                            <v-card>
+                                <v-card-title class="text-h5">
+                                    Detalle de Venta
+                                </v-card-title>
+                                <v-card-text>
+                                    <v-row>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="CLIENTE" v-model="editedItemVentas.customers_name"
+                                                readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="METODO PAGO" v-model="editedItemVentas.payment_method"
+                                                readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="COMPROBANTE" v-model="editedItemVentas.proof_payment"
+                                                readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="MONEDA" v-model="editedItemVentas.coin" readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="Tipo de cambio"
+                                                v-model="editedItemVentas.exchange_rate" readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="TOTAL" v-model="editedItemVentas.total" readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="FECHA" v-model="editedItemVentas.date" readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="3">
+                                            <v-text-field label="ESTADO" v-model="editedItemVentas.state_name" readonly>
+                                            </v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                    <v-card elevation="2">
+                                        <v-card-text>
+                                            <v-simple-table fixed-header height="250px">
+                                                <template v-slot:default>
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="text-left"> PRODUCTO </th>
+                                                            <th class="text-left"> CANTIDAD </th>
+                                                            <th class="text-left"> PRECIO </th>
+                                                            <th class="text-left"> IGV </th>
+                                                            <th class="text-left"> SUB TOTAL </th>
+                                                        </tr>
+                                                    </thead>
+
+                                                    <tbody>
+                                                        <tr v-for="item in editedItemVentas.details"
+                                                            :key="item.products_id">
+                                                            <td>{{ item.product_name }}</td>
+                                                            <td>{{ item.quantity }}</td>
+                                                            <td>{{ item.price }}</td>
+                                                            <td>{{ item.igv }}</td>
+                                                            <td>{{ item.subTotal }}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </template>
+                                            </v-simple-table>
+                                        </v-card-text>
+                                    </v-card>
+
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="green darken-1" text @click="dialog_view = false">
+                                        Cerrar
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </v-row>
+                </template>
+                <!-- Fin Ver detalles Ventas -->
+
                 <!-- Ganancias del dia -->
-                <v-tab-item>
-                    <v-data-table :headers="comprasDia" :items="desserts" sort-by="name" class="elevation-24"
+                <v-tab-item> <br>
+                    <v-data-table :headers="comprasDia" :items="dessertsVentas" sort-by="name" class="elevation-24"
                         :search="search">
                     </v-data-table>
                 </v-tab-item>
             </v-tabs>
         </v-card>
+        <pre>{{ valores }}</pre>
     </admin-layout>
 </template>
 <script>
 import AdminLayout from '@/Layouts/AdminLayout'
 export default {
-    name: 'hello',
+    props: [
+        'orders',
+        'purchases',
+        'totalVentas',
+        'totOrders',
+        'valores',
+    ],
     components: {
         AdminLayout,
     },
     data() {
         return {
             search: '',
+            dialog_view: false,
+
+            // VENTAS
             ventasDia: [
-                { text: 'NOMBRE', value: 'name' },
-                { text: 'DESCRIPCION', value: 'description' },
+                { text: 'FECHA', value: 'date' },
+                { text: 'CLIENTE', value: 'customers_name' },
+                { text: 'TOTAL', value: 'total' },
+                { text: 'MONEDA', value: 'coin' },
+                { text: 'ESTADO', value: 'state_name' },
                 { text: 'ACCIONES', value: 'actions', sortable: false },
             ],
+            dessertsVentas: [],
+            editedIndexVentas: -1,
+            editedItemVentas: {
+                id: '',
+                companies_id: '',
+                company_name: '',
+                customers_id: '',
+                customers_name: '',
+                payment_methods_id: '',
+                payment_method: '',
+                proof_payments_id: '',
+                proof_payment: '',
+                coins_id: '',
+                coin: '',
+                documents_id: '',
+                documents_name: '',
+                exchange_rate: '',
+                total: '',
+                date: '',
+                state: '',
+                state_name: '',
+                description: '',
+                details: '',
+            },
+
+            defaultItemVentas: {
+                id: '',
+                companies_id: '',
+                company_name: '',
+                customers_id: '',
+                customers_name: '',
+                payment_methods_id: '',
+                payment_method: '',
+                proof_payments_id: '',
+                proof_payment: '',
+                coins_id: '',
+                coin: '',
+                documents_id: '',
+                documents_name: '',
+                exchange_rate: '',
+                total: '',
+                date: '',
+                state: '',
+                state_name: '',
+                description: '',
+                details: '',
+            },
+
+            // COMPRAS
+
             comprasDia: [
-                { text: 'hola', value: 'name' },
-                { text: 'hola', value: 'description' },
+                { text: 'FECHA', value: 'name' },
+                { text: 'CLIENTE', value: 'description' },
+                { text: 'TOTAL', value: 'total' },
+                { text: 'ESTADO', value: 'state_name' },
                 { text: 'ACCIONES', value: 'actions', sortable: false },
             ],
+            dessertsCompras: [],
+            editedIndexCompras: -1,
         }
     },
+    created() {
+        this.initialize();
+
+    },
     methods: {
+        initialize() {
+            this.dessertsVentas = this.orders
+            // this.dessertsCompras = this.orders
+        },
+        viewItem(item) {
+            this.editedIndexVentas = this.dessertsVentas.indexOf(item)
+            this.editedItemVentas = Object.assign({}, item)
+            this.dialog_view = true
+        },
     },
 }
 </script>
