07083647d63b49fef6d259dc1936131b335a0186
ivanrv23 <ronalirv23@gmail.com>
2022-10-18 08:38:26 -0500

    reportes

--- resources/js/Pages/Reports/Index.vue
+++ resources/js/Pages/Reports/Index.vue
@@ -1,7 +1,30 @@
 <template>
     <admin-layout>
-        <v-card color="blue lighten-5">
-            <v-tabs fixed-tabs icons-and-text color="green lighten-1">
+        <v-card>
+            <v-card-title>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <!-- <v-menu v-model="calendarioConsulta" :close-on-content-click="false" :nudge-right="20"
+                    transition="scale-transition" offset-y min-width="auto">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-text-field class="mt-2" outlined dense v-model="date" label="Fecha de consulta"
+                            prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on">
+                        </v-text-field>
+                    </template>
+                    <v-date-picker locale="es-ES" v-model="date" @change="changeDate"
+                        @input="calendarioConsulta = false"></v-date-picker>
+                </v-menu> -->
+                <v-text-field type="date" class="mt-8 ma-2" outlined dense v-model="dateInicio" label="Fecha Inicio">
+                </v-text-field>
+                <v-text-field type="date" class="mt-8 ma-2" outlined dense v-model="dateFin" label="Fecha Fin">
+                </v-text-field>
+                <v-btn @click="ConsultaFechas" small color="primary" dark>
+                    consultar
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+            </v-card-title>
+            <v-tabs fixed-tabs icons-and-text color="green lighten-1" class="mt-0">
                 <v-tab>
                     VENTAS DEL DÍA
                 </v-tab>
@@ -11,6 +34,9 @@
                 <v-tab>
                     PRODUCTOS
                 </v-tab>
+                <br>
+                <v-card>
+                </v-card>
                 <!-- Ventas del día -->
                 <v-tab-item><br>
                     <v-card elevation="0">
@@ -76,34 +102,6 @@
                         :search="searchV">
                         <template v-slot:top>
                             <v-toolbar flat>
-                                <v-toolbar-title>
-                                    <v-text-field type="date" class="mt-8 ma-2" outlined dense
-                                    v-model="date" @change="changeDate"
-                                    label="Fecha"
-                                    >
-                                    </v-text-field>
-                                </v-toolbar-title>
-
-                                <!-- <v-menu v-model="menu2" :close-on-content-click="false" :nudge-right="40"
-                                    transition="scale-transition" offset-y min-width="auto" >
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-text-field
-                                            v-model="date"
-                                            label="Fecha"
-                                            readonly
-                                            v-bind="attrs"
-                                            v-on="on"
-                                            outlined
-                                            dense
-                                        ></v-text-field>
-                                    </template>
-                                    <v-date-picker
-                                        v-model="date"
-                                        @input="menu2 = false"
-                                    ></v-date-picker>
-                                </v-menu> -->
-
-
                                 <v-spacer></v-spacer>
                                 <v-text-field v-model="searchV" append-icon="mdi-magnify" label="Buscar" outlined dense
                                     hide-details>
@@ -464,11 +462,11 @@ export default {
             searchV: '',
             searchC: '',
             searchP: '',
+            calendarioConsulta: false,
             dialog_viewVentas: false,
             dialog_viewCompras: false,
             dateInicio: '',
             dateFin: '',
-
             // VENTAS
             ventasDia: [
                 { text: 'FECHA', value: 'date' },
@@ -502,7 +500,6 @@ export default {
                 description: '',
                 details: '',
             },
-
             defaultItemVentas: {
                 id: '',
                 companies_id: '',
@@ -525,7 +522,6 @@ export default {
                 description: '',
                 details: '',
             },
-
             // COMPRAS
             comprasDia: [
                 { text: 'FECHA', value: 'date' },
@@ -557,7 +553,6 @@ export default {
                 description: '',
                 details: '',
             },
-
             defaultItemCompras: {
                 id: '',
                 companies_id: '',
@@ -588,14 +583,11 @@ export default {
                 { text: 'FECHA DE VENCIMIENTO', value: 'expiration_date' },
             ],
             dessertsProducts: [],
-
             editedIndexProducts: -1,
-
         }
     },
     created() {
         this.initialize();
-
     },
     methods: {
         initialize() {
@@ -613,9 +605,8 @@ export default {
             this.editedItemCompras = Object.assign({}, item)
             this.dialog_viewCompras = true
         },
-        changeDate(){
-            alert('asd')
-            this.$inertia.get('/dashboard/reports/' + this.date)
+        ConsultaFechas() {
+            this.$inertia.get('/dashboard/reports/' + this.dateInicio + '/' + this.dateFin)
         },
     },
 }
